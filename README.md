106633fae4fa8b090d018744d1f9d09fecac8869
DominickDejesus <34385118+DominickDejsus@users.noreply.github.com>
2020-11-11 17:08:50 -0500

    added args, error checking, functions for switching add or remove mode, and updated readme

--- README.md
+++ README.md
@@ -1,27 +1,45 @@
 # Google Drive Permission Script
 
-## Setup
+## Setup for development
 
-- Go to https://console.developers.google.com/
+1. Go to https://console.developers.google.com/
 
-- Create/select a project.
+2. Create/select a project.
 
-- Enable the Google Drive API.
+3. Enable the Google Drive API.
 
-- Go to https://console.developers.google.com/apis/credentials and create a service account for the project.
+4. Go to https://console.developers.google.com/apis/credentials and create a service account for the project.
 
-- Click Create Credentials -> Service Account -> Type a name for the account -> Create.
+5. Click Create Credentials -> Service Account -> Type a name for the account -> Create.
 
-- Click on the newly created account, then the add key button.
+6. Click on the newly created account, then the add key button.
 
-- Select create new and generate the file in JSON.
+7. Select create new and generate the file in JSON.
 
-- Rename the file `credentials.json` and add it to the project directory.
+8. Rename the file `credentials.json` and add it to the project directory.
 
-- Give editing permision to the `client_email` on the folder that you want to control using this script. This `client_email` is the one refered to in the JSON file that was generated.
+9. Give editing permision to the `client_email` on the folder that you want to control using this script. This `client_email` is the one refered to in the JSON file that was generated.
+
+## Setup for running
+
+- Follow steps 7-9 to give the service account authorization to manage the google drive.
 
 ## Running the Script
 
+To add wrtie permissions to a list of email adresses
+
 ```bash
-node .
+node index.js -a gdriveFileName ./pathToTextFileWithEmails.txt
+```
+
+To remove write permissions to a list of email adresses:
+
+```
+node index.js -r gdriveFileName ./pathToTextFileWithEmails.txt
+```
+
+To get an example of the argments to use:
+
+```
+node index.js -h
 ```
--- index.js
+++ index.js
@@ -1,114 +1,166 @@
 const { google } = require("googleapis");
-const fs = require("fs");
-const async = require("async");
+const fs = require("fs").promises;
+//the credentials that you download for the service account on google console
 const credentials = require("./credentials.json");
 
 const scopes = ["https://www.googleapis.com/auth/drive"];
 
 const auth = new google.auth.JWT(
-	credentials.client_email,
-	null,
-	credentials.private_key,
-	scopes
+  credentials.client_email,
+  null,
+  credentials.private_key,
+  scopes
 );
-
 const drive = google.drive({ version: "v3", auth });
-let res;
-(async function () {
-	res = await drive.files.list({
-		q: "name='c55'",
-		fields: "files(name,id)",
-		orderBy: "createdTime desc",
-	});
-	console.log(res.data);
 
-	let fileId = res.data.files[0].id;
-	let students = [
-		{
-			type: "user",
-			role: "writer",
-			emailAddress: "nick@wyncode.co",
-		},
-	];
-	aaddPermissions(fileId, students);
-	const permList = await drive.permissions.list({
-		fields: "permissions(emailAddress,id)",
-		fileId: fileId,
-	});
-	console.log(permList.data.permissions);
+//entry point
+(async () => {
+  try {
+    const args = process.argv.slice(2);
+
+    await checkCliArgs(args);
+    const emails = await getAccountEmails(args[2]);
+    const file = await getFileId(args[1]);
+
+    if (args[0] === "-r") {
+      const permIds = await getPermissionIds(file, emails);
+      await removePermissions(file, permIds);
+    } else if (args[0] === "-a") {
+      await addPermissions(file, emails);
+    } else {
+      throw new Error({ error: "Wrong cli arguments!" });
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
 })();
 
-function getPermissionIds() {}
+/**
+ * Reads a text file of emails and returns an array.
+ * @param {array} emailsFile
+ * @returns {array} the emails
+ */
+async function getAccountEmails(emailsFile) {
+  try {
+    const data = await fs.readFile(emailsFile);
+    array = data.toString().replace(/\r\n/g, "\n").split("\n");
+    console.log("Emails array: ", array);
+    return array;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
 
+/**
+ * Gets the fileId that the service account can access.
+ * @param {string} fileName
+ * @returns {string} Id of the file as string
+ */
+async function getFileId(fileName) {
+  const res = await drive.files.list({
+    q: `name='${fileName}'`,
+    fields: "files(name,id)",
+    orderBy: "createdTime desc",
+  });
+  console.log("Found this file: ", res.data.files);
+
+  return res.data.files[0].id;
+}
+
+/**
+ * Gets a list of ids and emails of everyone that has permissions for a file.
+ * @param {string} fileId
+ * @param {array} emails an array of emails to get permissions for.
+ * @returns {array} an array of objects with emailAddress and id properties
+ */
+async function getPermissionIds(fileId, emails) {
+  const {
+    data: { permissions },
+  } = await drive.permissions.list({
+    fields: "permissions(emailAddress,id)",
+    fileId: fileId,
+  });
+  const filteredPermissions = permissions
+    .filter((permObj) => {
+      return emails.includes(permObj.emailAddress);
+    })
+    .map((permObj) => {
+      return permObj.id;
+    });
+  console.log("Permissions id array ", filteredPermissions);
+  return filteredPermissions;
+}
+
+/**
+ * Removes the given permesssions of for the specified file.
+ * @param {string} fileId
+ * @param {array} permissionIds an array of ids
+ */
 function removePermissions(fileId, permissionIds) {
-	permissionIds.forEach((permissionId) => {
-		drive.permissions.delete(
-			{
-				fileId: fileId,
-				permissionId: permissionId,
-			},
-			function (err, res) {
-				if (err) {
-					// Handle error...
-					console.error(err);
-				} else {
-					console.log("Permission ID: ", res.data.id);
-				}
-			}
-		);
-	});
+  permissionIds.forEach((permissionId) => {
+    drive.permissions.delete(
+      {
+        fileId: fileId,
+        permissionId: permissionId,
+      },
+      function (err, res) {
+        if (err) {
+          // Handle error...
+          console.error(err);
+        } else {
+          console.log("Account removed Permission ID");
+        }
+      }
+    );
+  });
 }
 
-async function addPermissions(fileId, students) {
-	students.forEach(async (student) => {
-		await drive.permissions.create(
-			{
-				resource: student,
-				fileId: fileId,
-				fields: "id",
-			},
-			function (err, res) {
-				if (err) {
-					// Handle error...
-					console.error(err);
-				} else {
-					console.log("Account added Permission ID: ", res.data.id);
-				}
-			}
-		);
-	});
+/**
+ * Adds permission to a file.
+ * @param {string} fileId
+ * @param {array} accounts an array of emails.
+ */
+async function addPermissions(fileId, accounts) {
+  accounts.forEach(async (account) => {
+    await drive.permissions.create(
+      {
+        resource: {
+          type: "user",
+          role: "writer",
+          emailAddress: account,
+        },
+        fileId: fileId,
+        fields: "id",
+      },
+      function (err, res) {
+        if (err) {
+          // Handle error...
+          console.error(err);
+        } else {
+          console.log("Account added Permission ID: ", res.data.id);
+        }
+      }
+    );
+  });
 }
 
-// function addPermissions(fileId, permissions) {
-// 	async.eachSeries(
-// 		permissions,
-// 		function (permission, permissionCallback) {
-// 			drive.permissions.create(
-// 				{
-// 					resource: permission,
-// 					fileId: fileId,
-// 					fields: "id",
-// 				},
-// 				function (err, res) {
-// 					if (err) {
-// 						// Handle error...
-// 						console.error(err);
-// 						permissionCallback(err);
-// 					} else {
-// 						console.log("Permission ID: ", res.data.id);
-// 						permissionCallback();
-// 					}
-// 				}
-// 			);
-// 		},
-// 		function (err) {
-// 			if (err) {
-// 				// Handle error
-// 				console.error(err);
-// 			} else {
-// 				// All permissions inserted
-// 				console.log("success");
-// 			}
-// 		}
-// 	);
-// }
+async function checkCliArgs(argArr) {
+  switch (argArr[0]) {
+    case "-r":
+    case "-a":
+      break;
+    case "-h":
+      throw new Error(
+        "Sample input: node index.js gdrivefilename ./fileToEmailsList.txt"
+      );
+    default:
+      throw new Error(
+        "Bad argument: epxecting -r or -a! For sample input use: node index.js -h"
+      );
+  }
+
+  if (!argArr[1])
+    throw new Error("Bad argument: expecting a google drive file name");
+
+  const stat = await fs.lstat(argArr[2]);
+}
